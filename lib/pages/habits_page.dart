e5026f3eb18c253825644eadd313cf059cd2f076
vijay996 <vijay0996@proton.me>
2025-09-07 21:11:58 +0530

    wip urgent commit

--- lib/pages/habits_page.dart
+++ lib/pages/habits_page.dart
@@ -1,30 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:lhtmd3/components/date_tile.dart';
+import 'package:lhtmd3/models/habit.dart';
+import 'package:lhtmd3/models/habit_entry.dart';
+import 'package:lhtmd3/models/habit_with_entries.dart';
+import 'package:lhtmd3/services/database.dart';
 
-class Habits extends StatelessWidget {
+class Habits extends StatefulWidget {
   const Habits({super.key});
 
   @override
+  State<Habits> createState() => _HabitsState();
+}
+
+class _HabitsState extends State<Habits> {
+  @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.sizeOf(context).width;
+    int maxIcons = ((width - 200) / 45).floor().clamp(1, 100);
+    final databaseService = DatabaseService();
+    final habitsList = databaseService.getHabitsWithEntries(maxIcons);
     return ListView(
       children: [
         // TODO: send dates list
         DateTile(),
-        // TODO: write a for loop with text, and state of each date tile
-        HabitTile(
-          habitName: 'did not complete',
-          habitStates: [0,1,0,1,2],
-          habitToggle: () {},
-        ),
-        HabitTile(
-          habitName: 'completed',
-          habitStates: [0,1,2,1,2],
-          habitToggle: () {},
-        ),
-        HabitTile(
-          habitName: 'skipped',
-          habitStates: [0,1,1,1,0],
-          habitToggle: () {},
+        FutureBuilder<List<HabitWithEntries>>(
+          future: habitsList,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator(); // Or some other loading indicator
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return Text('No habits found.');
+            } else {
+              return Column(
+                children: snapshot.data!.map((habit) {
+                  return HabitTile(
+                    habitName: habit.habit.habitName,
+                    habitId: habit.habit.habitId!,
+                    habitType: habit.habit.habitType,
+                    habitEntries: habit.entries,
+                  );
+                }).toList(),
+              );
+            }
+          },
         ),
       ],
     );
@@ -33,14 +53,16 @@ class Habits extends StatelessWidget {
 
 class HabitTile extends StatefulWidget {
   final String habitName;
-  final List<int> habitStates;
-  final VoidCallback habitToggle;
+  final int habitId;
+  final HabitType habitType;
+  final List<HabitEntry> habitEntries;
 
   const HabitTile({
     super.key,
     required this.habitName,
-    required this.habitStates,
-    required this.habitToggle,
+    required this.habitId,
+    required this.habitType,
+    required this.habitEntries,
   });
 
   @override
@@ -54,31 +76,39 @@ class _HabitTileState extends State<HabitTile> {
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        int maxIcons = ((constraints.maxWidth - 200) / 45).floor().clamp(1, 100);
-        
+        // int maxIcons = ((constraints.maxWidth - 200) / 45).floor().clamp(1, 100);
         return ListTile(
           contentPadding: EdgeInsets.only(left: 16.0, right: 8.0),
           title: Text(widget.habitName),
           leading: Icon(Icons.incomplete_circle),
           trailing: Wrap(
-            // spacing: 10,
             children: [
-              for (int i = 0; i < maxIcons; i++) ...[
+              for(final entry in widget.habitEntries)
                 SizedBox(
                   height: 48,
                   width: 48,
-                  child: IconButton(
-                    onPressed: () { setState(() {
-                      // TODO: update the database & icon
-                    }); }, 
-                    icon: Icon(widget.habitStates[i] == 0 ? Icons.close : widget.habitStates[i] == 1 ? Icons.check : Icons.remove)
-                  ),
+                  child: widget.habitType == HabitType.yesNo
+                    ? IconButton(onPressed: () async {
+                      final databaseService = DatabaseService();
+                      final entry = HabitEntry(
+                        entryId: entryId, 
+                        habitId: habitId, 
+                        entryDate: entryDate, 
+                        value: value
+                      );
+                    }, icon: _getIconForEntry(entry.value))
+                    : Text('temp'),
                 ),
-              ],
             ],
           ),
         );
       },
     );
   }
+}
+
+Widget _getIconForEntry(double value) {
+  return Icon(
+    value == 0 ? Icons.close : value == 1 ? Icons.check : Icons.remove,
+  );
 }
