41322227543e8f0a42d967f8ee4207d39a624212
vijay996 <vijay0996@proton.me>
2025-09-11 21:34:59 +0530

    reset option for debugging

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:lhtmd3/services/database.dart';
 
 class Settings extends StatelessWidget {
   const Settings({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    // TODO: implement settings
+    final databaseService = DatabaseService();
+    return ListTile(
+      leading: Icon(Icons.delete),
+      title: Text('Reset the app'),
+      onTap: databaseService.deleteDatabase,
+    );
   }
 }
